2231bdafc0ce9102285fa7594673729ada39317e
Vitor Cristiano Fellizatti <111813682+Vicrisfell@users.noreply.github.com>
2023-11-27 14:36:11 -0300

    Add files via upload

--- Banco de dados/portaria.sql
+++ Banco de dados/portaria.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1deb5ubuntu1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Tempo de geração: 31-Out-2023 às 01:39
--- Versão do servidor: 10.4.13-MariaDB
--- versão do PHP: 7.4.8
+-- Host: localhost:3306
+-- Tempo de geração: 16/10/2023 às 01:30
+-- Versão do servidor: 8.0.33-0ubuntu0.22.04.2
+-- Versão do PHP: 8.1.2-1ubuntu2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,21 +20,22 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `portaria`
 --
-
+CREATE DATABASE portaria;
+USE portaria;
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `registro`
+-- Estrutura para tabela `registro`
 --
 
 CREATE TABLE `registro` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `data_hora` varchar(20) NOT NULL,
-  `veiculos_id` int(11) DEFAULT NULL
+  `veiculos_id` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Extraindo dados da tabela `registro`
+-- Despejando dados para a tabela `registro`
 --
 
 INSERT INTO `registro` (`id`, `data_hora`, `veiculos_id`) VALUES
@@ -51,18 +52,17 @@ INSERT INTO `registro` (`id`, `data_hora`, `veiculos_id`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `veiculos`
+-- Estrutura para tabela `veiculos`
 --
 
 CREATE TABLE `veiculos` (
-  `id` mediumint(9) NOT NULL,
+  `id` mediumint NOT NULL,
   `aluno` varchar(50) NOT NULL,
-  `placa` varchar(20) NOT NULL,
-  
+  `placa` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Extraindo dados da tabela `veiculos`
+-- Despejando dados para a tabela `veiculos`
 --
 
 INSERT INTO `veiculos` (`id`, `aluno`, `placa`) VALUES
@@ -70,44 +70,39 @@ INSERT INTO `veiculos` (`id`, `aluno`, `placa`) VALUES
 (2, 'Pietro Porto', 'YFE-8L59'),
 (3, 'Joana Jesus', 'KJP-7M28'),
 (4, 'Letícia Rocha', 'HEM-9V76'),
-(5, 'Alícia Freitas', 'UYY-9C19'),
-(6, 'vitor', 'ete5645'),
-(7, 'vitor cristiano fellizatt', 'ete5645'),
-(8, 'orland', 'ffr8526'),
-(9, 'vitor', 'ete5645'),
-(10, 'vitor cristiano ', 'higwsgvghwsvhd');
+(5, 'Alícia Freitas', 'UYY-9C19');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices para tabela `registro`
+-- Índices de tabela `registro`
 --
 ALTER TABLE `registro`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices para tabela `veiculos`
+-- Índices de tabela `veiculos`
 --
 ALTER TABLE `veiculos`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `registro`
 --
 ALTER TABLE `registro`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de tabela `veiculos`
 --
 ALTER TABLE `veiculos`
-  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` mediumint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
